f582fd39cad0e3fdf8ca479c732c823b0ca1c69a
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-25 13:34:31 -0700

    updated data array and Panel component in the render

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,27 +2,34 @@ import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
+import axios from "axios";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay
+ } from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6
+    getValue: getTotalInterviews
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm"
+    getValue: getLeastPopularTimeSlot
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday"
+    getValue: getMostPopularDay
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3"
+    getValue: getInterviewsPerDay
   }
 ];
 
@@ -31,8 +38,11 @@ const data = [
 class Dashboard extends Component {
 
   state = { 
-    loading: false, 
-    focused: null
+    loading: true, 
+    focused: null,
+    days: [],
+    appointments: {},
+    interviewers: {}
   };
 
 
@@ -48,6 +58,19 @@ class Dashboard extends Component {
     if (focused) {
       this.setState( {focused});
     }
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data
+      });
+    });
   }
 
   componentDidUpdate(previousProps, previousState) {
@@ -63,6 +86,7 @@ class Dashboard extends Component {
     });
     
     if (this.state.loading) {
+      console.log(this.state.loading)
       return <Loading />;
     }
     
@@ -72,10 +96,10 @@ class Dashboard extends Component {
       )
       .map(panel => ( 
       <Panel
-      key={panel.id} 
-      label={panel.label}
-      value={panel.value}
-      onSelect={event => this.selectPanel(panel.id)}
+        key={panel.id} 
+        label={panel.label}
+        value={panel.getValue(this.state)}
+        onSelect={() => this.selectPanel(panel.id)}
       />
     ));
 
