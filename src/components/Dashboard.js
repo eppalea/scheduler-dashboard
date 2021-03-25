e4d9441dea0e475ecc55d4f9d861bc5d31b2b7d1
Eppa Lea Turniawan <eppalea@gmail.com>
2021-03-24 20:14:16 -0700

    added a focused key to the state obj, a .filter to the panels variable, and conditional css to the dashboardClasses variable

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -30,17 +30,26 @@ const data = [
 
 class Dashboard extends Component {
 
-  state = { loading: false };
+  state = { 
+    loading: false, 
+    focused: null
+  };
 
   
   render() { 
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
     
     if (this.state.loading) {
       return <Loading />;
     }
     
-    const panels = data.map(panel => ( 
+    const panels = data
+      .filter(
+        panel => this.state.focused === null || this.state.focused === panel.id
+      )
+      .map(panel => ( 
       <Panel
       key={panel.id} 
       id={panel.id}
